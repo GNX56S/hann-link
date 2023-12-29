69248a4efb14b49ede9f33a7f2db131e7a1f7dd9
Hannn <ilhanjuliansyah3@gmail.com>
2023-12-29 17:50:49 +0700

    memindahkan tag html

--- src/Components/ButtonLink.jsx
+++ src/Components/ButtonLink.jsx
@@ -2,15 +2,15 @@
 const ButtonLink = (props) => {
   return (
     <>
+    <a href={props.tujuan}>
         <button className="flex flex-col items-center justify-between px-3 py-2 text-center text-xl font-bold text-black rounded-xl w-32 h-32 m-auto hover:shadow-lg">
             <img src={props.logo} alt="logo" className="w-20" />
-            <a href={props.tujuan}>
-              <span className="">
+            <span className="">
                 {props.nama}
-              </span>
-            </a>
+            </span>
             {/* <img src="/src/assets/right-arrow_271228.png" alt="arrow" className="w-4"/> */}
         </button>
+    </a>
     </>
   );
 };
